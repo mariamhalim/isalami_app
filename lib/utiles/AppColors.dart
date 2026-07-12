6ae0b7b333888e557366ef48e58dd8bafe8b3116
Mariam Osama <mrmrosos86@gmail.com>
2026-07-12 21:37:17 +0300

    +Initialize project +Add empty home screen +Add AppColors & AppRoutes

---
+++ lib/utiles/AppColors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/animation.dart';
+
+class AppColors {
+  static const Color grayColor = Color(0xFF707070);
+  static const Color goldColor = Color(0xFFE2BE7F);
+  static const Color blackColor = Color(0xFF202020);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color brownColor = Color(0xFF946107);
+  static const Color offWhiteColor = Color(0xFFFFF5E3);
+}
---
+++ lib/utiles/AppRouters.dart
@@ -0,0 +1,3 @@
+class AppRoutes{
+  static const String HomeRouteName = 'Home Screen';
+}
