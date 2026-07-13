b7c5467ff56b771455a41a638c09f4295f0e327e
Mariam Osama <mrmrosos86@gmail.com>
2026-07-13 03:17:40 +0300

    +Add splash_screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
     initialRoute: AppRoutes.HomeRouteName,
       routes:{
        AppRoutes.HomeRouteName:(context) => HomeScreen(),
